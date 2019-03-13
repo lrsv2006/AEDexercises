da145e4bb48558cfaf4d72c557fda4088127084c
Lucas Rodrigues <43002509+lrsv2006@users.noreply.github.com>
2019-03-13 00:06:57 -0300

    Add files via upload

--- Pilha.java
+++ Pilha.java
@@ -1,55 +1,113 @@
-package List1Question1;
 import java.util.*;
 
 class Pilha {
 
-
-
+    private Pilha top;
     private Pilha next;
-    private Object pilhaelement;
-    static Pilha topo;
+    private int size;
+    private int length;
+    private char bracket;
+    private Pilha previous;
+
 
     Pilha() {
         this.next = null;
-        this.pilhaelement = null;
-        this.topo = null;
-    }
-}
-
-interface PilhaInterface {
-
-    public void push(Pilha pilhabracket, char bracket);
-    public Object pop();
-    public Object top();
-    public boolean isEmpty();
-
-}
-
-class PilhaRep implements PilhaInterface {
-
-    Pilha lista = new Pilha();
-
-
-    public void push(Pilha pilhabracket, char bracket) {
-
+        this.previous = null;
+        this.top = null;
+        this.bracket = 0;
+        this.size = 0;
+        this.length = 0;
 
     }
 
-    public Object pop() {
-        return null;
+    public void setBracket(char bracket) {
+        this.bracket = bracket;
     }
 
 
-    public Object top() {
-        return null;
+    public void setPrevious(Pilha previous) {
+        this.previous = previous;
     }
+
+    public Pilha getPrevious() {
+        return previous;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public char getBracket() {
+        return bracket;
+    }
+
+
+    public void push() {
+        if (top.getBracket() == 0) {
+            System.err.print("Não há nada para remover.");
+        } else {
+            top = top.getPrevious();
+        }
+
+    }
+
+    public void pop(char bracket) {
+        Pilha newbracket = new Pilha();
+        newbracket.setBracket(bracket);
+        newbracket.setPrevious(top);
+        top =  newbracket;
+        size = size++;
+
+    }
+
+    public void verify() {
+        if (top.getBracket() == '(') {
+            top.push();
+        }
+
+        if (top.getBracket() == ')') {
+            if (top.getPrevious().getBracket() == '(') {
+                length++;
+            } else {
+                previous.verify();
+            }
+        }
+    }
+
 
 
     public boolean isEmpty() {
-
+        if (top == null) {
+            return true;
+        }
         return false;
     }
+
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        Pilha pilha = new Pilha();
+
+        String bracketline = in.nextLine();
+
+        for (int i = 0; i < bracketline.length(); i++) {
+            pilha.pop(bracketline.charAt(i));
+        }
+
+        for (int i = 0; i < bracketline.length(); i++) {
+            if (bracketline.charAt(0) ==')') {
+
+            }
+        }
+
+        pilha.verify();
+
+    }
 }
+
+
+
+
 
 
 
