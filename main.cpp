675ebd6e632c297e0ba943b3b937daf15fff577a
Lucas Rodrigues <43002509+lrsv2006@users.noreply.github.com>
2019-03-21 12:09:45 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 
 int main() {
-    int everySize[100];
+
     int childrenAmount;
     cin >> childrenAmount;
+    int everySize[childrenAmount];
     int childrenSize;
 
     int i = 0;
@@ -18,21 +19,57 @@ int main() {
         }
     }
 
-    for (int i = 1; i < childrenAmount; i++) {
-        int value = everySize[i];
-        int pastvalue = i-1;
-        while (pastvalue >= 0 && everySize[pastvalue] > value) {
-            everySize[pastvalue+1] = everySize[pastvalue];
-            pastvalue--;
+            for (int i = 1; i < childrenAmount; i++) {
+                int value = everySize[i];
+                int pastvalue = i-1;
+                while (pastvalue >= 0 && everySize[pastvalue] > value) {
+                    everySize[pastvalue+1] = everySize[pastvalue];
+                    pastvalue--;
+                }
+                everySize[pastvalue+1] = value;
         }
 
-        everySize[pastvalue+1] = value;
-    }
-
-    for (int i = 0; i < childrenAmount; i++) {
-        cout << everySize[i] << endl;
-    }
+            int everySize2[(sizeof(everySize))/4];
 
 
-    return 0;
+        everySize2[childrenAmount / 2] = everySize[childrenAmount - 1];
+
+        if (childrenAmount % 2 != 0) {
+            for (int i = 0, j = i + 1, ii = 0, jj = 1;
+                 j < childrenAmount - 1; i++, j++, ii++, jj++) {
+
+                everySize2[childrenAmount - jj] = everySize[i];
+                everySize2[ii] = everySize[j];
+                i++;
+                j++;
+
+            }
+        } else {
+            for (int i = 0, j = i + 1, ii = 0, jj = 1;
+                 j < childrenAmount - 1; i++, j++, ii++, jj++) {
+
+                if(ii != childrenAmount/2) {
+
+                    everySize2[childrenAmount - jj] = everySize[i];
+                    everySize2[ii] = everySize[j];
+                    i++;
+                    j++;
+                } else {
+                    ii++;
+                    everySize2[ii] = everySize[j];
+                }
+            }
+        }
+
+            for (int i = 0; i < childrenAmount; i++) {
+                cout << everySize2[i] + " ";
+            }
+
+
+
+
+
+
+
+
 }
